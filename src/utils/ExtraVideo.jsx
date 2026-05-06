ec2be9e342e07b556baf77b381aef140a3a747c5
bhupeshjoshi119 <bhupesh.coding@gmail.com>
2026-05-06 12:45:51 +0530

    added ine video

--- src/utils/ExtraVideo.jsx
+++ src/utils/ExtraVideo.jsx
@@ -3,7 +3,8 @@ const videoMap={
     "Is it worth learning React?":"https://www.youtube.com/watch?v=Cmm-t0E8YXQ",
     "End of Vibe Coding? Github Copilot Changes":"https://www.youtube.com/watch?v=TKal4pZHr0U",
     "Github are you joking?":"https://www.youtube.com/watch?v=b13m-iuu4XU",
-    "AI Is Killing GitHub":"https://www.youtube.com/watch?v=y73qW8TcMVU"
+    "AI Is Killing GitHub":"https://www.youtube.com/watch?v=y73qW8TcMVU",
+    "Mastering Python Generators a FizzBuzz Case Study":"https://www.youtube.com/watch?v=ylNUm6i5_Is"
 }
 
 export default  videoMap;
